8d87b2cfaf94116d271712db07d28ff6888f28ed
sjzhao <sjun.zhao@gmail.com>
2013-10-14 17:32:11 +0800

    add ng-iscroller comphones

--- angular/demo/js/app.js
+++ angular/demo/js/app.js
@@ -1,16 +1,43 @@
-function HelloController ($scope) {
-  $scope.greeting = {text: "Hello"}
-}
+//function HelloController ($scope) {
+//  $scope.greeting = {text: "Hello"}
+//}
+//
+//function CartController ($scope) {
+//
+//  $scope.items = [
+//    {title: "Paint pots", price: 3.95, quantity: 8},
+//    {title: "Polka dots", price: 12.95, quantity: 17},
+//    {title: "Pebbles", price: 6.95, quantity: 5}
+//  ];
+//
+//  $scope.remove = function (index) {
+//    $scope.items.splice(index, 1);
+//  }
+//}
+var webapp = angular.module('zinchapp', ['ng-iscroll']);
 
-function CartController ($scope) {
+webapp.controller('scroll', function ($scope) {
 
-  $scope.items = [
-    {title: "Paint pots", price: 3.95, quantity: 8},
-    {title: "Polka dots", price: 12.95, quantity: 17},
-    {title: "Pebbles", price: 6.95, quantity: 5}
-  ];
+	var schools = [];
+	var schoolitem = {
+		chinese_name: '普林斯顿大学',
+		name: 'Princeton University',
+		location: 'Princeton, New Jersey',
+		hot: '计算机, 工程'
+	}
 
-  $scope.remove = function (index) {
-    $scope.items.splice(index, 1);
-  }
-}
+	for (var i = 1; i <= 50; i++) {
+		schools[i] = schoolitem;
+	}
+
+	$scope.schools = schools;
+
+//	$scope.$parent.myScrollOptions = {
+//		snap: false,
+//		onScrollEnd: function ()
+//		{
+//			alert('finshed scrolling');
+//		}
+//	};
+
+});
